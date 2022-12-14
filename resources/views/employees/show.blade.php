38e606cf7e6e1e64d1bafca6724852c9d638f438
Mohammad Arkan <87922247+MArkan1919@users.noreply.github.com>
2022-12-14 08:12:12 +0700

    Update show.blade.php

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -11,36 +11,27 @@
                     <div class="card-header">Dashboard</div>
                     <div class="card-body">
                         <div class="table-responsive">
-                            <form action="{{ route('employees.show', $employee->id) }}" method="POST">
-                                @csrf
-
-                                <div class="mb-3 text-center">
-                                    <img src="{{ asset('storage/' . $employee->image) }}" class="rounded" width="50%">
-                                </div>
-
-                                <div class="mb-3">
-                                    <label class="form-label">Name</label>
-                                    <input type="text" class="form-control" name="name" value="{{ $employee->name }}" readonly>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label class="form-label">Email</label>
-                                    <input type="text" class="form-control" name="email" value="{{ $employee->email }}" readonly>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label class="form-label">Gender</label>
-                                    <input type="text" class="form-control" name="gender" value="{{ $employee->gender }}" readonly>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label class="form-label">Address</label>
-                                    <input type="text" class="form-control" name="address" value="{{ $employee->address }}" readonly>
-                                </div>
-
-                                <hr>
-                                <a href="{{ route('employees.index') }}" class="btn btn-danger btn-block">Back</a>
-                            </form>
+                            <div class="mb-3 text-center">
+                                <img src="{{ asset('storage/' . $employee->image) }}" class="rounded" width="50%">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Name</label>
+                                <input type="text" class="form-control" name="name" value="{{ $employee->name }}" readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Email</label>
+                                <input type="text" class="form-control" name="email" value="{{ $employee->email }}" readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Gender</label>
+                                <input type="text" class="form-control" name="gender" value="{{ $employee->gender }}" readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Address</label>
+                                <input type="text" class="form-control" name="address" value="{{ $employee->address }}" readonly>
+                            </div>
+                            <hr>
+                            <a href="{{ route('employees.index') }}" class="btn btn-danger btn-block">Back</a>
                         </div>
                     </div>
                 </div>
